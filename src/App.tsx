abdf35d18b38afa16c96a88ac06b79de20ce5122
rob <robertaxelkirby@gmail.com>
2019-05-01 13:07:30 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+// import './App.scss';
+
+export default function App() {
+  return (
+    <div className="App">Hello world</div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// Note: currently set up for dev only
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: {
+    "bundle.min": "./src/index.tsx",
+  },
+
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "/build")
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+  ],
+};
